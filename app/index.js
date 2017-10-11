2467d75bb20e751da255e08b5430e01401d21834
Gabrielle Saman <gabi.duarte@gmail.com>
2017-10-11 15:44:44 -0300

    Add readline to process input

--- app/index.js
+++ app/index.js
@@ -1,3 +1,14 @@
-console.log("Up and running");
+const rl = require('readline');
+
+const prompts = rl.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
+
+prompts.question("Insira o tipo de cliente e o range de datas", (answer) => {
+	prompts.close();
+});
+
+
 
 
