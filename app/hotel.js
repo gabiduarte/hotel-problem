468424fa226a66e5c9cad29bb684bc960ba6960c
Gabrielle Saman <gabi.duarte@gmail.com>
2017-10-31 14:45:26 -0200

    Create hotel and hotelRate structure

---
+++ app/hotel.js
@@ -0,0 +1,10 @@
+class Hotel {
+	constructor(name, rating, rates) {
+		this.name = name;
+		this.rating = rating;
+		this.rates = rates;
+	}
+
+}
+
+module.exports = Hotel;
---
+++ app/hotelRates.js
@@ -0,0 +1,8 @@
+class HotelRates {
+	constructor(regular, rewards) {
+		this.regular = regular;
+		this.rewards = rewards;
+	}
+}
+
+module.exports = HotelRates;
